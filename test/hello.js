cec79be3ef6cd6d4d8ccbff90c27077bc32f49ca
vspedr <vspedrazzi@gmail.com>
2017-03-07 13:42:38 -0300

    travis pre build scripts

---
+++ test/hello.js
@@ -0,0 +1,17 @@
+process.env.NODE_ENV = 'TEST';
+
+const request = require('request');
+const should = require('should');
+const mocha = require('mocha');
+
+describe('/GET Hello', () => {
+  it('should get Hello Travis', (done) => {
+    request({
+      url: 'http://localhost:1337',
+      method: 'get',
+    }, (error, response) => {
+      response.should.have.property('body', 'Hello Travis!\n');
+      done();
+    });
+  });
+});
